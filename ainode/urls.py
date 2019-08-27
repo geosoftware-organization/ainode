7de0511395ec62c5a3a468f273f861328b7e8354
Dobromir Kovachev <dobromir.mail@gmail.com>
2019-08-27 23:35:05 +0300

    Changes for REST API and add model and serializer for Users

--- ainode/urls.py
+++ ainode/urls.py
@@ -13,9 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets, routers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet, basename=None, base_name=None)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^', include(router.urls), kwargs=None, name=None),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

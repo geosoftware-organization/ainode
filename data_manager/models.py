226567cdb4bc290972794154a9776f4c2515e65e
Dobromir Kovachev <dobromir.mail@gmail.com>
2019-12-24 17:17:23 +0200

    Additional data_sets

--- data_manager/models.py
+++ data_manager/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.name} {self.code}'
+
--- data_manager/views.py
+++ data_manager/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from .models import Country
 
 # Create your views here.

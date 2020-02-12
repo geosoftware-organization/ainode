afdceaae616c87595d9f87682b3fa4b8d39514dd
Dobromir Kovachev <dobromir.mail@gmail.com>
2020-02-12 21:38:12 +0200

    Make models in data_manager app visible in admin form

--- data_manager/admin.py
+++ data_manager/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Share)
+class ShareAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.DailyStockPrice)
+class DailyStockPriceAdmin(admin.ModelAdmin):
+    pass
